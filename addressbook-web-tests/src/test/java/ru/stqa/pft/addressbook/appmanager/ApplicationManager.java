da65408923795aec883166dd7e7971595f349e0a
riikki <riikkimarley@gmail.com>
2016-04-21 07:45:22 -0700

    Сделан класс ApplicationManager и перенесены вспомогательные методы в него,

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,23 +1,15 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
+public class ApplicationManager {
 	FirefoxDriver wd;
-
-	@BeforeMethod
-	public void setUp() throws Exception {
-		wd = new FirefoxDriver();
-		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		wd.get("http://localhost/addressbook/");
-		login("admin", "secret");
-	}
 
 	public static boolean isAlertPresent(FirefoxDriver wd) {
 		try {
@@ -28,7 +20,14 @@ public class TestBase {
 		}
 	}
 
-	private void login(String username, String password) {
+	public void init() {
+		wd = new FirefoxDriver();
+		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		wd.get("http://localhost/addressbook/");
+		login("admin", "secret");
+	}
+
+	public void login(String username, String password) {
 		wd.findElement(By.name("user")).click();
 		wd.findElement(By.name("user")).clear();
 		wd.findElement(By.name("user")).sendKeys(username);
@@ -38,15 +37,15 @@ public class TestBase {
 		wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
 	}
 
-	protected void goToGroupPage() {
+	public void goToGroupPage() {
 		wd.findElement(By.linkText("group page")).click();
 	}
 
-	protected void submitGroupCreation() {
+	public void submitGroupCreation() {
 		wd.findElement(By.name("submit")).click();
 	}
 
-	protected void fillGroupForm(GroupData groupData) {
+	public void fillGroupForm(GroupData groupData) {
 		wd.findElement(By.name("group_name")).click();
 		wd.findElement(By.name("group_name")).clear();
 		wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
@@ -58,33 +57,33 @@ public class TestBase {
 		wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
 	}
 
-	protected void deletedSelectedGroups() {
+	public void deletedSelectedGroups() {
 		wd.findElement(By.name("delete")).click();
 	}
 
-	protected void selectGroup() {
+	public void selectGroup() {
 		if (!wd.findElement(By.name("selected[]")).isSelected()) {
 			wd.findElement(By.name("selected[]")).click();
 		}
 	}
 
-	protected void initGroupCreation() {
+	public void initGroupCreation() {
 		wd.findElement(By.name("new")).click();
 	}
 
-	protected void gotoGroupPage() {
+	public void gotoGroupPage() {
 		wd.findElement(By.linkText("groups")).click();
 	}
 
-	protected void goToHomePage() {
+	public void goToHomePage() {
 		wd.findElement(By.linkText("home")).click();
 	}
 
-	protected void submitContactCreation() {
+	public void submitContactCreation() {
 		wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
 	}
 
-	protected void fillContactForm(ContactData contactData) {
+	public void fillContactForm(ContactData contactData) {
 		wd.findElement(By.name("firstname")).click();
 		wd.findElement(By.name("firstname")).clear();
 		wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
@@ -105,14 +104,11 @@ public class TestBase {
 		wd.findElement(By.name("email")).sendKeys(contactData.getMail());
 	}
 
-	protected void initContactCreation() {
+	public void initContactCreation() {
 		wd.findElement(By.linkText("add new")).click();
 	}
 
-	@AfterMethod
-	public void tearDown() {
+	public void stop() {
 		wd.quit();
 	}
-
-
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase {
+
+	@Test
+	public void testContactCreationTests() {
+		app.initContactCreation();
+		app.fillContactForm(new ContactData("TestFirstName", "TestMiddleName", "TestLastName", "TestNickname", "+111111111111", "testmail@test.com"));
+		app.submitContactCreation();
+		app.goToHomePage();
+	}
+}
+
+
