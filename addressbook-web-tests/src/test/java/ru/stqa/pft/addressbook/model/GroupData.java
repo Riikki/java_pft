5823458750e77ea54005db9195ba1dd132db19f1
Golubovich Dmitry <riikkimarley@gmail.com>
2016-05-11 15:48:41 +0200

    Повсеместное использование уникальных идентификаторов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -59,6 +59,7 @@ public class GroupData {
 
 		GroupData groupData = (GroupData) o;
 
+		if (id != groupData.id) return false;
 		return name != null ? name.equals(groupData.name) : groupData.name == null;
 
 	}
