684841ec8e10b1811da1b2dfe5f3bb98fa58c69d
riikki <riikkimarley@gmail.com>
2016-05-24 09:38:00 -0700

    Формат JSON JavaScript Object Notation

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 import java.io.File;
@@ -41,9 +43,19 @@ public class GroupDataGenerator {
             saveAsCsv(groups, new File(file));
         }else if (format.equals("xml")) {
             saveAsCsv(groups, new File(file));
+        }else if (format.equals("json")) {
+            saveAsJson(groups, new File(file));
         }else {
             save(groups, new File(file));
         }
+    }
+
+    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
     }
 
     private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
@@ -67,7 +79,7 @@ public class GroupDataGenerator {
     private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++) {
-            groups.add(new GroupData().withName(String.format("test\n%s",i)).withHeader(String.format("header\n%s",i)).withFooter(String.format("footer\n%s",i)));
+            groups.add(new GroupData().withName(String.format("test %s",i)).withHeader(String.format("header %s",i)).withFooter(String.format("footer %s",i)));
         }
         return groups;
     }
