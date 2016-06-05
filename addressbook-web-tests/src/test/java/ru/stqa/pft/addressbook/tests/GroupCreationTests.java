002702ef1e396a5c231840104aa20d28d50698a4
riikki <riikkimarley@gmail.com>
2016-06-05 18:19:54 +0300

    Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -20,6 +20,7 @@ public class GroupCreationTests extends TestBase {
 		Groups after = app.db().groups();
 		assertThat(after, equalTo(
 				before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+		verifyGroupListInUI();
 	}
 
 
@@ -33,6 +34,7 @@ public class GroupCreationTests extends TestBase {
 		Groups after = app.db().groups();
 		assertThat(after,equalTo(
 				before.withoutAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+		verifyGroupListInUI();
 	}
 
 }
