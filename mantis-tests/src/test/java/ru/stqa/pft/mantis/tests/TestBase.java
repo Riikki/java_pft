686ef8da0f706052a98d2a49d45246f13dd1e2cb
riikki <riikkimarley@gmail.com>
2016-06-11 22:38:47 +0300

    Задание №21: Реализовать интеграцию тестов с баг-трекером MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,14 +1,19 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 import ru.stqa.pft.mantis.model.MailMessage;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public class TestBase {
@@ -39,5 +44,15 @@ public class TestBase {
 
 	}
 
+	public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+		if (isIssueOpen(issueId)) {
+			throw new SkipException("Ignored because of issue " + issueId);
+		}
+	}
+
+	private boolean isIssueOpen(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+		IssueData issue = app.soap().getIssue(issueId);
+		return issue.getStatus().getId().intValue() != 90;
+	}
 
 }
