d42dbc09d8e466ff23833befafcc5efefb8bffc2
riikki <riikkimarley@gmail.com>
2016-06-12 19:42:39 +0300

    Удалённый запуск браузера (Selenium Server)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +15,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -41,13 +44,21 @@ public class ApplicationManager {
 
 		dbHelper = new DbHelper();
 
-		if (browser.equals(BrowserType.FIREFOX)) {
-			wd = new FirefoxDriver();
-		} else if (browser.equals(BrowserType.CHROME)) {
-			wd = new ChromeDriver();
-		} else if (browser.equals(BrowserType.IE)) {
-			wd = new InternetExplorerDriver();
+		if("".equals(properties.getProperty("selenium.server"))){
+			if (browser.equals(BrowserType.FIREFOX)) {
+				wd = new FirefoxDriver();
+			} else if (browser.equals(BrowserType.CHROME)) {
+				wd = new ChromeDriver();
+			} else if (browser.equals(BrowserType.IE)) {
+				wd = new InternetExplorerDriver();
+			}
+		}else{
+			System.out.printf("remote");
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setBrowserName(browser);
+			wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
 		}
+
 		wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 		wd.get(properties.getProperty("web.baseUrl"));
 		groupHelper = new GroupHelper(wd);
